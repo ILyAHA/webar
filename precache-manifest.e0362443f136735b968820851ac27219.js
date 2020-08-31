e2f2b882ee5ebb99ec8d41a193fc8b6d0d3ab54a
Ilya Vinogradov <i.vinogradov@cometrica.ru>
2020-08-31 09:29:12 +0300

    Updates

---
+++ precache-manifest.e0362443f136735b968820851ac27219.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771dc1024c52a609f1920f06f9c1c297",
+    "url": "/webar/index.html"
+  },
+  {
+    "revision": "75bd0adb9ef0eb9259a0",
+    "url": "/webar/static/css/main.1d173023.chunk.css"
+  },
+  {
+    "revision": "db6556d39a2189154cd6",
+    "url": "/webar/static/js/2.4d556eac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webar/static/js/2.4d556eac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75bd0adb9ef0eb9259a0",
+    "url": "/webar/static/js/main.013519c1.chunk.js"
+  },
+  {
+    "revision": "b7206d74bbbc52d1e265",
+    "url": "/webar/static/js/runtime-main.a0eefda4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webar/static/media/logo.5d5d9eef.svg"
+  }
+]);
